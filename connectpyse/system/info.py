19ed70e73856b41ae314159b6002cef0ad031e78
DLTADragonHawk <preston.helbling@gmail.com>
2025-08-05 20:18:41 -0500

    Added Info Api endpoint.

--- connectpyse/system/info.py
+++ connectpyse/system/info.py
@@ -4,9 +4,14 @@ from ..cw_model import CWModel
 class Info(CWModel):
 
     def __init__(self, json_dict=None):
-        self.version = None  # (String)
+        self.cloudRegion = None # (String)
         self.isCloud = None  # (Boolean)
+        self.licenseBits = None # (list(dict))
+        self.maxWorkFlowRecordsAllowed = None # Int
         self.serverTimeZone = None  # (String)
+        self.version = None  # (String)
+        
+
 
         # initialize object with json dict
         super().__init__(json_dict)
